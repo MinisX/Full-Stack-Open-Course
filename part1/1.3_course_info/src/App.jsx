c5ec990c82f6a9f3c8318e54ddbf450d89163b4a
MinisX <daniel.dedoukh@gmail.com>
2024-03-07 14:01:14 +0100

    exercise 1.3

---
+++ part1/1.3_course_info/src/App.jsx
@@ -0,0 +1,57 @@
+const Header = (props) => {
+  return(
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <div>
+      {props.parts.map((part, index) => (
+        <Part key={index} part={part.part} />
+      ))}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content 
+        parts={[{ part: part1 }, {part: part2}, {part: part3}]} 
+       />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
+    </div>
+  )
+}
+
+export default App
