37e73609bfeaa1b271f465dad5af9d254e602741
Daniel Dedoukh <daniel.dedoukh@axians-ikvs.de>
2024-09-28 16:47:17 +0200

    exercise 2.5

---
+++ part2/courseInfo/2.5/src/components/Course.js
@@ -0,0 +1,34 @@
+const Course = ({ course }) => {
+  const exercisesSum = course.parts.reduce(
+    (accumulator, part) => accumulator + part.exercises,
+    0
+  );
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={exercisesSum} />
+    </div>
+  );
+};
+
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </>
+);
+
+export default Course;
