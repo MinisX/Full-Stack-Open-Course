38e8c1264954bf64a7bccd3b8ab53f42fdf8cfff
Daniel Dedoukh <daniel.dedoukh@axians-ikvs.de>
2024-09-28 16:34:13 +0200

    exercise 2.2 and 2.3

---
+++ part2/courseInfo/2.3/src/App.js
@@ -0,0 +1,59 @@
+const Course = ({ course }) => {
+  const exercisesSum = course.parts.reduce(
+    (accumulator, part) => accumulator + part.exercises,
+    0
+  );
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={exercisesSum} />
+    </div>
+  );
+};
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </>
+);
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
