4683297de3d4ea65a38772c60384fbc6a2b070a1
MinisX <daniel.dedoukh@gmail.com>
2024-11-07 18:43:25 +0100

    exercise 3.10 adjusted froent vite config with proxy

--- part3/phonebook_backend/3.10/frontend/vite.config.js
+++ part3/phonebook_backend/3.10/frontend/vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  proxy: {
-    '/api': {
-      target: 'https://full-stack-open-course-ds7p.onrender.com',
-      changeOrigin: true,
-    },
-  }
+  plugins: [react()],server: {
+    proxy: {
+      '/api': {
+        target: 'https://full-stack-open-course-ds7p.onrender.com',
+        changeOrigin: true,
+      },
+    }
+  },
 })
