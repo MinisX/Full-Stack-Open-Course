2e92895e6a1f91478f57870addc71484033eaa69
MinisX <daniel.dedoukh@gmail.com>
2024-11-07 20:17:07 +0100

    exercise 3.11 changed the proxy url

--- part3/phonebook_backend/3.11/frontend/vite.config.js
+++ part3/phonebook_backend/3.11/frontend/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],server: {
+    proxy: {
+      '/api': {
+        target: 'https://full-stack-open-course-1.onrender.com',
+        changeOrigin: true,
+      },
+    }
+  },
 })
