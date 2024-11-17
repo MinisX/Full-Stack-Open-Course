acb205d6ca57bfba1a95cdbb5707b9a0a82b956c
MinisX <daniel.dedoukh@gmail.com>
2024-11-17 17:29:42 +0100

    exercise 4.11 init

---
+++ part4/blogList/4.11/backend/controllers/blogs.js
@@ -0,0 +1,20 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
+
+// catch of exception is handled by express-async-errors library
+blogsRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
+  const result = await blog.save()
+  response.status(201).json(result)
+})
+
+/*
+  A router object is an isolated instance of middleware and routes. You can think of it as a “mini-application,”
+  capable only of performing middleware and routing functions. Every Express application has a built-in app router.
+*/
+module.exports = blogsRouter
---
+++ part4/blogList/4.11/backend/tests/blog_api.test.js
@@ -0,0 +1,58 @@
+const { test, after, beforeEach } = require('node:test')
+const Blog = require('../models/blog')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+test('returns the correct amount of blog posts in the JSON format', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.equal(response.body.length, helper.initialBlogs.length)
+})
+
+test('unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+  response.body.forEach((blog) => {
+    assert(Object.keys(blog).includes('id'))
+  })
+})
+
+test('successfully creates a new blog post', async () => {
+  const newBlog = {
+    title: 'Georgy Blog',
+    author: 'Georgy',
+    url: 'georgu.com',
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsInDb = await helper.blogsInDb()
+  assert.strictEqual(blogsInDb.length, helper.initialBlogs.length + 1)
+
+  const titles = blogsInDb.map(blog => blog.title)
+  assert(titles.includes('Georgy Blog'))
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
