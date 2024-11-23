d505029ccaca5f44e669dee6678b35bb79a2d2c6
MinisX <daniel.dedoukh@gmail.com>
2024-11-23 21:31:22 +0100

    exercise 3.13 delete of blogs and test

---
+++ part4/blogList/4.13/backend/controllers/blogs.js
@@ -0,0 +1,25 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
+
+// catch of exception is handled by express-async-errors library
+blogsRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
+  const result = await blog.save()
+  response.status(201).json(result)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+/*
+  A router object is an isolated instance of middleware and routes. You can think of it as a “mini-application,”
+  capable only of performing middleware and routing functions. Every Express application has a built-in app router.
+*/
+module.exports = blogsRouter
---
+++ part4/blogList/4.13/backend/tests/blog_api.test.js
@@ -0,0 +1,127 @@
+const { test, after, beforeEach, describe } = require('node:test')
+const Blog = require('../models/blog')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+test('returns the correct amount of blog posts in the JSON format', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.equal(response.body.length, helper.initialBlogs.length)
+})
+
+test('unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+  response.body.forEach((blog) => {
+    assert(Object.keys(blog).includes('id'))
+  })
+})
+
+test('successfully creates a new blog post', async () => {
+  const newBlog = {
+    title: 'Georgy Blog',
+    author: 'Georgy',
+    url: 'georgu.com',
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsInDb = await helper.blogsInDb()
+  assert.strictEqual(blogsInDb.length, helper.initialBlogs.length + 1)
+
+  const titles = blogsInDb.map(blog => blog.title)
+  assert(titles.includes('Georgy Blog'))
+})
+
+
+test('likes property is missing from the request', async () => {
+  const newBlog = {
+    title: 'Kate Blog',
+    author: 'Kate',
+    url: 'kate.com',
+  }
+
+  const result = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(result.body.likes, 0)
+})
+
+test('title is missing missing', async () => {
+  const newBlog = {
+    author: 'Kate',
+    url: 'kate.com',
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('url is missing missing', async () => {
+  const newBlog = {
+    title: 'Kate Blog',
+    author: 'Kate',
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+})
+
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsBeforeDeletion = await helper.blogsInDb()
+    const blogToDelete = blogsBeforeDeletion[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAfterDeletion = await helper.blogsInDb()
+
+    assert.strictEqual(blogsAfterDeletion.length, blogsBeforeDeletion.length - 1)
+
+    const titles = blogsAfterDeletion.map(r => r.title)
+    assert(!titles.includes(blogToDelete.title))
+  })
+
+  test('fails with invalid id', async () => {
+    await api
+      .delete('/api/blogs/123123123')
+      .expect(400)
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
