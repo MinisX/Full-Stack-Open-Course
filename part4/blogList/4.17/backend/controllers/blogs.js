700bca5c433f4b4775eaa03dd1a40bbcf1c12d68
MinisX <daniel.dedoukh@gmail.com>
2024-12-04 21:25:18 +0100

    exercise 4.17 user contains blogs information, blogs contain user information

--- part4/blogList/4.17/backend/controllers/blogs.js
+++ part4/blogList/4.17/backend/controllers/blogs.js
@@ -1,15 +1,25 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 // catch of exception is handled by express-async-errors library
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+  const user = await User.findById(body.userId)
+
+  const blog = new Blog({
+    ...body, user: user.id
+  })
+
   const result = await blog.save()
+  user.blogs = user.blogs.concat(result._id)
+  await user.save()
+
   response.status(201).json(result)
 })
 
