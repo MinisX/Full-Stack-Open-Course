998f9f566caf179dbbf811c3064fc9f6f356a222
MinisX <daniel.dedoukh@gmail.com>
2024-12-17 19:06:27 +0100

    exercise 4.21 delete only for user who owns the blog

--- part4/blogList/4.21/backend/controllers/blogs.js
+++ part4/blogList/4.21/backend/controllers/blogs.js
@@ -29,8 +29,24 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
-  response.status(204).end()
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+
+  // if the entry does not exist, then it is still "removed"
+  if(blog === null || blog === undefined)
+    return response.status(204).end()
+
+  if (blog.user.toString() === decodedToken.id.toString() ){
+    await Blog.deleteOne()
+    return response.status(204).end()
+  }else{
+    return response.status(401).json({ error: 'invalid user' })
+  }
 })
 
 blogsRouter.put('/:id', async (request, response) => {
