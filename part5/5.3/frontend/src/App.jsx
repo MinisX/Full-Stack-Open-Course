ff0fb8ed4a2a87961edcb398361a7fc5d69a686d
MinisX <daniel.dedoukh@gmail.com>
2024-12-29 14:08:16 +0100

    exercise 5.3 init

---
+++ part5/5.3/frontend/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import Login from './components/Login'
+import blogService from './services/blogs'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null) 
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [user])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedNoteappUser')
+    setUser(null)
+  }
+
+  return (
+    <div>
+      {user === null 
+      ? 
+      <Login setUser={setUser}/> 
+      :
+      <div>
+        <h2>blogs</h2>
+        {user.name} logged in <button onClick={handleLogout}>logout</button>
+        <br/><br/>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    }
+    </div>
+  )
+}
+
+export default App
