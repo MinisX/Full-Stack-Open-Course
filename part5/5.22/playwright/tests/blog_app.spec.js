7d50de458c4cafd925206cf5282574174f4e7cdf
MinisX <daniel.dedoukh@gmail.com>
2025-03-12 22:09:35 +0100

    exercise 5.22 only the user who added the blog sees the delete butto

--- part5/5.22/playwright/tests/blog_app.spec.js
+++ part5/5.22/playwright/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { login, createNewBlog } = require('./helper')
 
 describe('Blog app', () => {
   const user = {
@@ -25,52 +26,29 @@ describe('Blog app', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-
-      await page.getByTestId('username').fill(user.username)
-      await page.getByTestId('password').fill(user.password)
-      await page.getByRole('button', { name: 'login' }).click()
-
+      await login(page, user.username, user.password)
       await expect(page.getByText('blogs')).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
-      
-      await page.getByTestId('username').fill(user.username.concat('a'))
-      await page.getByTestId('password').fill(user.password.concat('a'))
-      await page.getByRole('button', { name: 'login' }).click()
-
+      await login(page, user.username.concat('a'), user.password.concat('a'))
       await expect(page.getByText('Wrong credentials')).toBeVisible()
     })
   })
 
   describe('When logged in', () => {
     beforeEach(async ({ page }) => {
-      await page.getByTestId('username').fill(user.username)
-      await page.getByTestId('password').fill(user.password)
-      await page.getByRole('button', { name: 'login' }).click()
+      await login(page, user.username, user.password)
     })
   
     test('a new blog can be created', async ({ page }) => {
-      await page.getByRole('button', { name: 'create new blog' }).click()
-
-      const textboxes = await page.getByRole('textbox').all()
-      await textboxes[0].fill('title')
-      await textboxes[1].fill('author')
-      await textboxes[2].fill('url')
-      await page.getByRole('button', { name: 'create' }).click()
-
+      createNewBlog(page, 'title', 'author', 'url')
       await expect(page.getByText('title author')).toBeVisible()
     })
 
     
     test('blog can be liked', async ({ page }) => {
-      await page.getByRole('button', { name: 'create new blog' }).click()
-
-      const textboxes = await page.getByRole('textbox').all()
-      await textboxes[0].fill('title')
-      await textboxes[1].fill('author')
-      await textboxes[2].fill('url')
-      await page.getByRole('button', { name: 'create' }).click()
+      await createNewBlog(page, 'title', 'author', 'url')
       await page.getByRole('button', { name : 'view' }).click()
       await page.getByRole('button', { name : 'like' }).click()
 
@@ -78,13 +56,7 @@ describe('Blog app', () => {
     })
 
     test('a new blog can be deleted', async ({ page }) =>{
-      await page.getByRole('button', { name: 'create new blog' }).click()
-
-      const textboxes = await page.getByRole('textbox').all()
-      await textboxes[0].fill('title')
-      await textboxes[1].fill('author')
-      await textboxes[2].fill('url')
-      await page.getByRole('button', { name: 'create' }).click()
+      await createNewBlog(page, 'title', 'author', 'url')
       await page.getByRole('button', { name : 'view' }).click()
 
       await expect(page.getByText('title author')).toBeVisible()
@@ -97,6 +69,32 @@ describe('Blog app', () => {
 
       await page.getByRole('button', { name : 'remove' }).click()
       await expect(page.getByText('title author')).not.toBeVisible()
+    })
+
+    
+    test('only the user who added the blog sees the delete button', async ({ page, request }) => {
+      await createNewBlog(page, 'title', 'author', 'url')
+      await page.getByRole('button', {name : 'logout' }).click()
+
+      const user2 = {
+        name: 'Daniel Dondo2',
+        username: 'minisx2',
+        password: '123456789'
+      }
+    
+      await request.post('/api/users', {data: user2})
+      await login(page, user2.username, user2.password)
+      await createNewBlog(page, 'title2', 'author2', 'url2')
+      await page.getByText('title2 author2').waitFor()
+
+      const viewButton = await page.getByRole('button', { name : 'view' }).first()
+      const viewButton2 = await page.getByRole('button', { name : 'view' }).last()
+
+      await viewButton.click()
+      await viewButton2.click()
+
+      const deleteButtons = await page.getByRole('button', { name : 'remove' }).all()
+      expect(deleteButtons).toHaveLength(1);
     })
   })
 })
