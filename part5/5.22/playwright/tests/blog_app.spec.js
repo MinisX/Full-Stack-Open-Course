339363b3403774dc84f2e246c628a7cc9f035064
MinisX <daniel.dedoukh@gmail.com>
2025-03-12 21:07:00 +0100

    exercise 5.22 init

---
+++ part5/5.22/playwright/tests/blog_app.spec.js
@@ -0,0 +1,102 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  const user = {
+    name: 'Daniel Dondo',
+    username: 'minisx',
+    password: '123456789'
+  }
+
+  beforeEach(async ({ page, request }) => {
+    // it was await request.post('http://localhost:3001/api/testing/reset'), but in frontend we have redirect to 3001 when /api is used
+    // so http://localhost:5173/api redirects to localhost:3001
+
+    // we could also remove the localhost from here, because we defined it in playwright.config.js
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {data: user})
+    
+    await page.goto('/')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.locator('form')).toBeVisible()
+    await expect(page.getByText('log in to application')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+
+      await page.getByTestId('username').fill(user.username)
+      await page.getByTestId('password').fill(user.password)
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('blogs')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      
+      await page.getByTestId('username').fill(user.username.concat('a'))
+      await page.getByTestId('password').fill(user.password.concat('a'))
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('Wrong credentials')).toBeVisible()
+    })
+  })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByTestId('username').fill(user.username)
+      await page.getByTestId('password').fill(user.password)
+      await page.getByRole('button', { name: 'login' }).click()
+    })
+  
+    test('a new blog can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'create new blog' }).click()
+
+      const textboxes = await page.getByRole('textbox').all()
+      await textboxes[0].fill('title')
+      await textboxes[1].fill('author')
+      await textboxes[2].fill('url')
+      await page.getByRole('button', { name: 'create' }).click()
+
+      await expect(page.getByText('title author')).toBeVisible()
+    })
+
+    
+    test('blog can be liked', async ({ page }) => {
+      await page.getByRole('button', { name: 'create new blog' }).click()
+
+      const textboxes = await page.getByRole('textbox').all()
+      await textboxes[0].fill('title')
+      await textboxes[1].fill('author')
+      await textboxes[2].fill('url')
+      await page.getByRole('button', { name: 'create' }).click()
+      await page.getByRole('button', { name : 'view' }).click()
+      await page.getByRole('button', { name : 'like' }).click()
+
+      await expect(page.locator('.extra_content')).toContainText('1')
+    })
+
+    test('a new blog can be deleted', async ({ page }) =>{
+      await page.getByRole('button', { name: 'create new blog' }).click()
+
+      const textboxes = await page.getByRole('textbox').all()
+      await textboxes[0].fill('title')
+      await textboxes[1].fill('author')
+      await textboxes[2].fill('url')
+      await page.getByRole('button', { name: 'create' }).click()
+      await page.getByRole('button', { name : 'view' }).click()
+
+      await expect(page.getByText('title author')).toBeVisible()
+
+      // Handle the confirmation dialog
+      page.once('dialog', async (dialog) => {
+          expect(dialog.type()).toBe('confirm');
+          await dialog.accept();
+      });
+
+      await page.getByRole('button', { name : 'remove' }).click()
+      await expect(page.getByText('title author')).not.toBeVisible()
+    })
+  })
+})
