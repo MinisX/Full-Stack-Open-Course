2fbd7ce96177b2e4d41f72c06818591fb894e787
MinisX <daniel.dedoukh@gmail.com>
2025-03-23 12:18:10 +0100

    exercicse 6.3 init 2

--- part6/6.3/src/main.jsx
+++ part6/6.3/src/main.jsx
@@ -1,42 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import { createStore } from 'redux'
-import reducer from './reducer'
+import { Provider } from 'react-redux'
+import App from './App'
+import reducer from './reducers/anecdoteReducer'
 
 const store = createStore(reducer)
 
-const App = () => {
-  const actionTypes = {
-    good : 'GOOD',
-    ok : 'OK',
-    bad : 'BAD',
-    reset : 'ZERO'
-  }
-  const dispatchAction = (actionType) => {
-    store.dispatch({
-      type: actionType
-    })
-  }
-
-  return (
-    <div>
-      <button onClick={() => dispatchAction(actionTypes.good)}>good</button> 
-      <button onClick={() => dispatchAction(actionTypes.ok)}>ok</button> 
-      <button onClick={() => dispatchAction(actionTypes.bad)}>bad</button>
-      <button onClick={() => dispatchAction(actionTypes.reset)}>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok {store.getState().ok}</div>
-      <div>bad {store.getState().bad}</div>
-    </div>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const renderApp = () => {
-  root.render(<App />)
-}
-
-renderApp()
-store.subscribe(renderApp)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
