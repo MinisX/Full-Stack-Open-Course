582c6ea0a3d2d24dde0c8b6cd5c8de2f41533e15
MinisX <daniel.dedoukh@gmail.com>
2025-03-26 20:21:08 +0100

    exercise 6.20 retrieve anecdotes with React Query

--- part6/6.20/src/App.jsx
+++ part6/6.20/src/App.jsx
@@ -1,5 +1,7 @@
+import { useQuery } from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAnecdotes } from './services/anecdotes'
 
 const App = () => {
 
@@ -7,13 +9,20 @@ const App = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: false
+  })
+
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if(result.isLoading)
+    return <div>data is being loaded...</div>
+
+  if(result.isError){
+    return <div>anecdote service not available due to problems in server</div>
+  }
 
   return (
     <div>
@@ -22,7 +31,7 @@ const App = () => {
       <Notification />
       <AnecdoteForm />
     
-      {anecdotes.map(anecdote =>
+      {result.data.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
