578286005baf375698b56c977ba253a62bd81d9e
MinisX <daniel.dedoukh@gmail.com>
2025-03-26 20:38:18 +0100

    exercise 6.22 init

---
+++ part6/6.22/src/components/AnecdoteForm.jsx
@@ -0,0 +1,31 @@
+import { createAnecdote } from "../services/anecdotes"
+import { useQueryClient, useMutation } from '@tanstack/react-query'
+
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const newNoteMutation = useMutation({ 
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
+
+  const onCreate = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    newNoteMutation.mutate({ content, votes: 0 })
+}
+
+  return (
+    <div>
+      <h3>create new</h3>
+      <form onSubmit={onCreate}>
+        <input name='anecdote' />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
