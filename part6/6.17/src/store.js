7f751f45257c153d700b6840587b64dc7e8d38f3
MinisX <daniel.dedoukh@gmail.com>
2025-03-26 18:06:46 +0100

    exericse 6.17 small corrections, the creation of new anecdote was alreadz done in 6.16

--- part6/6.17/src/store.js
+++ part6/6.17/src/store.js
@@ -1,8 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-import anecdoteReducer, { setAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 import notificationReducer from './reducers/notificationReducer'
-import anecdoteService from './services/anecdotes'
 
 const store = configureStore({
   reducer: {
