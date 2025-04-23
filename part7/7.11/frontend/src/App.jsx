d1c00450890fc93ffccb008aef84245bd49855b0
MinisX <daniel.dedoukh@gmail.com>
2025-04-23 20:44:04 +0200

    exercise 7.11 init

---
+++ part7/7.11/frontend/src/App.jsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import Login from './components/Login';
+import BlogCreate from './components/BlogCreate';
+import blogService from './services/blogs';
+import Notification from './components/Notification';
+import './index.css';
+import Togglable from './components/Togglable';
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+
+  const sortBlogs = (blogsToSort) => {
+    return [...blogsToSort].sort((a, b) => a.likes - b.likes);
+  };
+
+  useEffect(() => {
+    if (user !== null) blogService.getAll().then((blogs) => setBlogs(sortBlogs(blogs)));
+  }, [user]);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
+  const updateBlog = (updatedBlog) => {
+    setBlogs((prevBlogs) => {
+      if (updatedBlog.deleted) return prevBlogs.filter((b) => b.id !== updatedBlog.id);
+
+      const newBlogs = prevBlogs.map((b) => (b.id === updatedBlog.id ? updatedBlog : b));
+      return sortBlogs(newBlogs);
+    });
+  };
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedNoteappUser');
+    setUser(null);
+  };
+
+  return (
+    <div>
+      <Notification />
+      {user === null ? (
+        <Login setUser={setUser} />
+      ) : (
+        <div>
+          <h2>blogs</h2>
+          {user.name} logged in <button onClick={handleLogout}>logout</button>
+          <br />
+          <br />
+          <Togglable buttonLabel="create new blog">
+            <h2>create new</h2>
+            <BlogCreate blogs={blogs} setBlogs={setBlogs} />
+          </Togglable>
+          <br />
+          <br />
+          {blogs.map((blog) => (
+            <Blog
+              key={blog.id}
+              blog={blog}
+              updateBlog={updateBlog}
+              showDeleteButton={blog.user.username === user.username}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
