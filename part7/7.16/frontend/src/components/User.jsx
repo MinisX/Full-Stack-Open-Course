548aab788cd551e43cc22b6af8b1f4a47489b679
MinisX <daniel.dedoukh@gmail.com>
2025-04-28 10:57:59 +0200

    exercise 7.16 bug fix with User load when page relaoded

--- part7/7.16/frontend/src/components/User.jsx
+++ part7/7.16/frontend/src/components/User.jsx
@@ -1,10 +1,23 @@
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { initializeUsers } from '../reducers/usersReducer';
 
 const User = () => {
+  const dispatch = useDispatch();
   const users = useSelector(({ users }) => users);
   const id = useParams().id;
   const user = users.find((user) => user.id === id);
+
+  useEffect(() => {
+    if (users.length === 0) {
+      dispatch(initializeUsers()); // Fetch users if not already loaded
+    }
+  }, [users]);
+
+  if (!user) {
+    return null;
+  }
 
   return (
     <>
