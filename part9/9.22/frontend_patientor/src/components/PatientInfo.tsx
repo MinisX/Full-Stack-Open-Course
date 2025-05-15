02ff503c87bd9e0ffcf2ec0a7c8dc51473060571
minisx <daniel.dedoukh@gmail.com>
2025-05-15 14:34:45 +0200

    exercise 9.22 completed

---
+++ part9/9.22/frontend_patientor/src/components/PatientInfo.tsx
@@ -0,0 +1,42 @@
+import { useParams } from "react-router-dom";
+import { Patient } from "../types";
+import FemaleIcon from '@mui/icons-material/Female';
+import MaleIcon from '@mui/icons-material/Male';
+import NoAccountsIcon from '@mui/icons-material/NoAccounts';
+import { useEffect, useState } from "react";
+import patientService from '../services/patients';
+
+const PatientInfo = () => {
+    const id = useParams().id;
+    const [patient, setPatient] = useState<Patient>();
+
+    useEffect(() => {
+        if(id){
+            patientService.getById(id)
+            .then(data => setPatient(data));
+        }
+    }, []);
+
+     if(!patient)
+        return null;
+
+    return(
+        <>
+        <h1>
+            {patient.name}{" "}
+            {patient.gender === "male" ? (
+                <MaleIcon />
+            ) : patient.gender === "female" ? (
+                <FemaleIcon />
+            ) : (
+                <NoAccountsIcon />
+            )}
+        </h1>
+        {`ssn: ${patient.ssn}`}
+            <br/>
+            {`occupation: ${patient.occupation}`}
+        </>
+    );
+};
+
+export default PatientInfo;
