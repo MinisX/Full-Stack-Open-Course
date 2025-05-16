005ca0dbc881fb8864811019c409ef8711e49e67
minisx <daniel.dedoukh@gmail.com>
2025-05-16 09:22:16 +0200

    exercise 9.25 in progress

---
+++ part9/9.25/backend/src/utils.ts
@@ -0,0 +1,56 @@
+import { Gender, HealthCheckRating } from "./types";
+import { z } from "zod";
+
+const healthCheckEntrySchema = z.object({
+  id: z.string(),
+  description: z.string(),
+  date: z.string(),
+  specialist: z.string(),
+  diagnosisCodes: z.array(z.string()).optional(),
+  type: z.literal("HealthCheck"),
+  healthCheckRating: z.nativeEnum(HealthCheckRating),
+});
+
+const occupationalHealthcareEntrySchema = z.object({
+  id: z.string(),
+  description: z.string(),
+  date: z.string(),
+  specialist: z.string(),
+  diagnosisCodes: z.array(z.string()).optional(),
+  type: z.literal("OccupationalHealthcare"),
+  employerName: z.string(),
+  sickLeave: z
+    .object({
+      startDate: z.string(),
+      endDate: z.string(),
+    })
+    .optional(),
+});
+
+const hospitalEntrySchema = z.object({
+  id: z.string(),
+  description: z.string(),
+  date: z.string(),
+  specialist: z.string(),
+  diagnosisCodes: z.array(z.string()).optional(),
+  type: z.literal("Hospital"),
+  discharge: z.object({
+    date: z.string(),
+    criteria: z.string(),
+  }),
+});
+
+const entrySchema = z.union([
+  healthCheckEntrySchema,
+  occupationalHealthcareEntrySchema,
+  hospitalEntrySchema,
+]);
+
+export const newEntrySchema = z.object({
+  name: z.string(),
+  dateOfBirth: z.string(),
+  ssn: z.string(),
+  gender: z.nativeEnum(Gender),
+  occupation: z.string(),
+  entries: z.array(entrySchema), // Use the entry schema here
+});
---
+++ part9/9.25/frontend_patientor/src/App.tsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { Button, Divider, Container, Typography } from '@mui/material';
+
+import { apiBaseUrl } from "./constants";
+import { Patient } from "./types";
+
+import diagnosisService from "./services/diagnosis"
+import patientService from "./services/patients";
+import PatientListPage from "./components/PatientListPage";
+import PatientInfo from "./components/PatientInfo";
+
+const App = () => {
+  const [patients, setPatients] = useState<Patient[]>([]);
+  const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([]);
+
+  useEffect(() => {
+    void axios.get<void>(`${apiBaseUrl}/ping`);
+
+    const fetchPatientList = async () => {
+      const patients = await patientService.getAll();
+      setPatients(patients);
+    };
+    void fetchPatientList();
+
+    const fetchDiagnosistList = async () => {
+      const diagnoses = await diagnosisService.getAll();
+      setDiagnoses(diagnoses);
+    };
+
+    void fetchPatientList();
+    void fetchDiagnosistList();
+  }, []);
+  
+  return (
+    <div className="App">
+      <Router>
+        <Container>
+          <Typography variant="h3" style={{ marginBottom: "0.5em" }}>
+            Patientor
+          </Typography>
+          <Button component={Link} to="/" variant="contained" color="primary">
+            Home
+          </Button>
+          <Divider hidden />
+          <Routes>
+            <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
+            <Route path="/patients/:id" element={<PatientInfo diagnoses={diagnoses}/>}/>
+          </Routes>
+        </Container>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
